c42451d5ef51dce791cab3ffbc8ef243a00d3950
giuliadiiass <giuliadias2708@gmail.com>
2023-05-31 18:38:35 -0300

    oi

---
+++ src/main/java/net/giulia/cadastro_de_trecos/Cadastro_de_trecos.java
@@ -0,0 +1,88 @@
+package net.giulia.cadastro_de_trecos;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import net.giulia.cadastro_de_trecos.setup.AppSetup;
+import net.giulia.cadastro_de_trecos.crud.Read;
+import net.giulia.cadastro_de_trecos.crud.Create;
+import net.giulia.cadastro_de_trecos.crud.Delete;
+import net.giulia.cadastro_de_trecos.crud.Search;
+import net.giulia.cadastro_de_trecos.crud.Update;
+
+public class Cadastro_de_trecos extends AppSetup {
+
+    public static void main(String[] args) {
+        clearScreen();
+        mainMenu();
+    }
+
+    // Método que exibe o menu principal.
+    public static void mainMenu() {
+
+        System.out.println(appName + "\n" + appSep);
+        System.out.println("Menu:");
+        System.out.println("\t[1] Listar todos");
+        System.out.println("\t[2] Listar");
+        System.out.println("\t[3] Novo");
+        System.out.println("\t[4] Editar");
+        System.out.println("\t[5] Apagar");
+        System.out.println("\t[6] Procurar");
+        System.out.println("\t[0] Sair");
+        System.out.println(appSep);
+        System.out.print("Opção: ");
+
+        // Recebe a opção do teclado.
+        String option = scanner.next();
+
+        // Executa um método conforme a opção escolhida.
+        switch (option) {
+            case "0":
+                exitProgram();
+                break;
+            case "1":
+                clearScreen();
+                Read.readAll();
+                break;
+            case "2":
+                clearScreen();
+                Read.read();
+                break;
+            case "3":
+                clearScreen();
+                Create.create();
+                break;
+            case "4":
+                clearScreen();
+                Update.update();
+                break;
+            case "5":
+                clearScreen();
+                Delete.delete();
+                break;
+            case "6":
+                clearScreen();
+                Search.search();
+                break;
+            default:
+                clearScreen();
+                System.out.println("Oooops! Opção inválida!\n");
+                mainMenu();
+        }
+    }
+
+    // Encerra o programa.
+    public static void exitProgram() {
+        scanner.close();
+        clearScreen();
+        System.out.println("\n\nFui!\n\n");
+        System.exit(0);
+    }
+
+    // Limpa a tela do terminal.
+    public static void clearScreen() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("\n");
+        }
+    }
+
+}
---
+++ src/main/java/net/giulia/cadastro_de_trecos/crud/Read.java
@@ -0,0 +1,173 @@
+package net.giulia.cadastro_de_trecos.crud;
+
+import java.sql.SQLException;
+import net.giulia.cadastro_de_trecos.setup.AppSetup;
+import net.giulia.cadastro_de_trecos.db.DbConnection;
+import static net.giulia.cadastro_de_trecos.setup.AppSetup.*;
+import static net.giulia.cadastro_de_trecos.Cadastro_de_trecos.*;
+import static net.giulia.cadastro_de_trecos.Tools.showRes;
+
+public class Read extends AppSetup {
+
+    // Lista todos os registros.
+    public static void readAll() {
+
+        // Reserva recursos.
+        String sql;
+
+        // Cabeçalho da view.
+        System.out.println(appName + "\n" + appSep);
+        System.out.println("Lista todos os registros");
+        System.out.println(appSep + "\n");
+
+        try {
+
+            // Consulta o banco de dados.
+            sql = "SELECT * FROM " + DBTABLE;
+            conn = DbConnection.dbConnect();
+            stmt = conn.createStatement();
+            res = stmt.executeQuery(sql);
+            if (res.next()) {
+
+                // Se encontrou registros, exibe na view.
+                do {
+                    showRes(res);
+                } while (res.next());
+            } else {
+
+                // Se não encontrou registro.
+                clearScreen();
+                System.out.println("Oooops! Não achei nada!\n");
+            }
+
+            // Fecha recursos.
+            DbConnection.dbClose(res, stmt, pstm, conn);
+
+            // Menu inferior da seção.
+            System.out.println(appSep);
+            System.out.println("Menu:\n\t[1] Menu principal\n\t[0] Sair");
+            System.out.println(appSep);
+
+            // Recebe opção do teclaso.
+            System.out.print("Opção: ");
+            String option = scanner.next();
+
+            // Executa conforme a opção.
+            switch (option) {
+                case "0":
+                    exitProgram();
+                    break;
+                case "1":
+                    clearScreen();
+                    mainMenu();
+                    break;
+                default:
+                    clearScreen();
+                    System.out.println("Oooops! Opção inválida!\n");
+                    readAll();
+            }
+
+        } catch (SQLException error) {
+
+            // Tratamento de erros.
+            System.out.println("Oooops! " + error.getMessage());
+            System.exit(0);
+        }
+
+    }
+
+    // Lista um único registro pelo id.
+    public static void read() {
+
+        // Reserva recursos para o banco de dados.
+        int id = 0;
+        String sql;
+
+        // Cabeçalho da seção.
+        System.out.println(appName + "\n" + appSep);
+        System.out.println("Lista um registro");
+        System.out.println(appSep);
+
+        try {
+
+            // Recebe o Id do teclado.
+            System.out.print("Digite o ID ou [0] para retornar: ");
+            id = Integer.parseInt(scanner.next());
+            if (id == 0) {
+                clearScreen();
+                mainMenu();
+            }
+        } catch (NumberFormatException e) {
+
+            // Quando opção é inválida.
+            clearScreen();
+            System.out.println("Oooops! Opção inválida!\n");
+            read();
+        }
+
+        try {
+
+            System.out.println(" ");
+
+            // Faz consulta no banco de dados usando "preparedStatement".
+            sql = "SELECT * FROM " + DBTABLE + " WHERE id = ?";
+            conn = DbConnection.dbConnect();
+            pstm = conn.prepareStatement(sql);
+
+            // Passa o Id para a query.
+            pstm.setInt(1, id);
+
+            // Executa a query.
+            res = pstm.executeQuery();
+
+            if (res.next()) {
+                showRes(res);
+            } else {
+
+                // Se não tem registro.
+                clearScreen();
+                System.out.println("Oooops! Não achei nada!\n");
+                read();
+            }
+
+            // Fecha banco de dados.
+            DbConnection.dbClose(res, stmt, pstm, conn);
+
+            // Menu inferior da seção.
+            System.out.println(appSep);
+            System.out.println("Menu:\n\t[1] Menu principal\n\t[2] Listar outro\n\t[0] Sair");
+            System.out.println(appSep);
+
+            // Recebe opção do teclado.            
+            System.out.print("Opção: ");
+            String option = scanner.next();
+
+            // Executa conforme a opção.
+            switch (option) {
+                case "0":
+                    exitProgram();
+                    break;
+                case "1":
+                    clearScreen();
+                    mainMenu();
+                    break;
+                case "2":
+                    clearScreen();
+                    read();
+                    break;
+                default:
+                    clearScreen();
+                    System.out.println("Oooops! Opção inválida!\n");
+                    read();
+            }
+
+        } catch (SQLException error) {
+
+            // Tratamento de erros.
+            System.out.println("Oooops! " + error.getMessage());
+            System.exit(0);
+        }
+
+    }
+
+}
